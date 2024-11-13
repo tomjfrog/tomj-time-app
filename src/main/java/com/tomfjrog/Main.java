441460261494aabf8d3766b2e297b9993c16ee03
tomjfrog <tomj@jfrog.com>
2024-11-13 16:31:51 -0600

    Bump time-lib version

--- src/main/java/com/tomfjrog/Main.java
+++ src/main/java/com/tomfjrog/Main.java
@@ -5,6 +5,7 @@ import com.tomfjrog.time.TimeUtils;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println(TimeUtils.reportSupportedZones());
         String currentTime = TimeUtils.getCurrentSystemTime();
         System.out.println("Hello world!");
         System.out.println("The current time is: " + currentTime);
